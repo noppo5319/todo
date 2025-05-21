0e6285286dc2dd0e81b7690ef5a130e674ffa0fd
noppo5319 <noppo5319@gmail.com>
2025-05-21 17:56:31 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('todo-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/style.css',
+                '/script.js'
+            ]);
+        })
+    );
+    console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activated');
+});
